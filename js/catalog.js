738b7c299a0800793becee541d5fe070a41a6ccd
ecrandz <33246695+ecrandz@users.noreply.github.com>
2017-12-03 18:09:28 +0300

    Добавлен Javascript

---
+++ js/catalog.js
@@ -0,0 +1,63 @@
+// **************** определение объектов *********************
+// login
+var enter_button = document.querySelector(".header-enterlink");
+var not_logged = document.querySelector(".not-logged-in");
+var logged = document.querySelector(".logged-in");
+var logout_button = document.querySelector(".logout-icon");
+// купить
+var buy_button = document.querySelectorAll(".buy-button");
+var basket_popup = document.querySelector(".basket");
+var basket_close = document.querySelector(".basket-close");
+var header_basket = document.querySelector(".header-basket");
+var continue_selection = document.querySelector(".basket-button-inactive");
+// добавить в избранное
+var to_favorities_button = document.querySelectorAll(".to-favorities-button");
+var header_favorities = document.querySelector(".header-shortcuts");
+// счётчики покупок
+var purchase_counter = 0;
+var favorities_counter = 0;
+var purchase_counter_indicator = document.querySelector(".header-basket span");
+var favorities_counter_indicator = document.querySelector(".header-shortcuts span");
+
+// ********************** действия ****************************
+
+// login
+enter_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  not_logged.classList.remove("show");
+  logged.classList.add("show");
+});
+logout_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  logged.classList.remove("show");
+  not_logged.classList.add("show");
+});
+
+//купить
+for(var i = 0; i < buy_button.length; i++) {
+  buy_button[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    basket_popup.classList.add("show");
+    header_basket.classList.add("header-item-active");
+    purchase_counter = purchase_counter + 1;
+    purchase_counter_indicator.innerHTML= purchase_counter;
+  });
+}
+basket_close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  basket_popup.classList.remove("show");
+});
+continue_selection.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  basket_popup.classList.remove("show");
+});
+
+// добавить в избранное
+for(var i = 0; i < to_favorities_button.length; i++) {
+  to_favorities_button[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    header_favorities.classList.add("header-item-active");
+    favorities_counter = favorities_counter + 1;
+    favorities_counter_indicator.innerHTML= favorities_counter;
+  });
+}
